24a104b38b80e0f13466b4148701002d9cbe56a5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-19 19:57:15 +0900

    Cameramanを制御出来るように

---
+++ Assets/HK/AutoAnt/Scripts/Cameraman.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt
+{
+    /// <summary>
+    /// カメラマン
+    /// </summary>
+    public sealed class Cameraman : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform root;
+
+        [SerializeField]
+        private Transform pivot;
+
+        [SerializeField]
+        private Transform rig;
+
+        [SerializeField]
+        private Transform distance;
+
+        [SerializeField]
+        private Camera controlledCamera;
+        public Camera Camera => this.controlledCamera;
+
+        public static Cameraman Instance { get; private set; }
+
+        void Awake()
+        {
+            Assert.IsNull(Instance);
+            Instance = this;
+        }
+
+        void OnDestroy()
+        {
+            Assert.IsNotNull(Instance);
+            Instance = null;
+        }
+    }
+}
