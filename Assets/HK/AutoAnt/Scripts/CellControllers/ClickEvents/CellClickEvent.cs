d833651aa3f634f7672e31710a066f922b23f971
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-19 22:38:09 +0900

    GameSystem実装

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/ClickEvents/CellClickEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.CellControllers.ClickEvents
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class CellClickEvent : ScriptableObject, ICellClickEvent
+    {
+        public void Do()
+        {
+        }
+    }
+}
---
+++ Assets/HK/AutoAnt/Scripts/Systems/GameSystem.cs
@@ -0,0 +1,31 @@
+﻿using HK.AutoAnt.CellControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.Systems
+{
+    /// <summary>
+    /// ゲームの根幹部分をまとめるクラス
+    /// </summary>
+    public sealed class GameSystem : MonoBehaviour
+    {
+        private static GameSystem instance;
+        public static GameSystem Instance => instance;
+
+        [SerializeField]
+        private CellManager cellManager;
+        public CellManager CellManager => this.cellManager;
+
+        void Awake()
+        {
+            Assert.IsNull(instance);
+            instance = this;
+        }
+
+        void OnDestroy()
+        {
+            Assert.IsNotNull(instance);
+            instance = null;
+        }
+    }
+}
