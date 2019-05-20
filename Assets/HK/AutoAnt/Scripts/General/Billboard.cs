6cdea494a5234c09dd4240314e1053247288248a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-21 00:48:47 +0900

    アイテムのアイコンがセルに表示されるように

---
+++ Assets/HK/AutoAnt/Scripts/General/Billboard.cs
@@ -0,0 +1,36 @@
+﻿using HK.AutoAnt.Systems;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt
+{
+    /// <summary>
+    /// カメラマンの方へ常に向くコンポーネント
+    /// </summary>
+    public sealed class Billboard : MonoBehaviour
+    {
+        [SerializeField]
+        private bool isReverse;
+
+        private Transform cachedTransform;
+
+        void Awake()
+        {
+            this.cachedTransform = this.transform;
+
+            this.LateUpdateAsObservable()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    var forward = -GameSystem.Instance.Cameraman.Camera.transform.forward;
+                    if(_this.isReverse)
+                    {
+                        forward *= -1.0f;
+                    }
+
+                    _this.cachedTransform.forward = forward;
+                });
+        }
+    }
+}
