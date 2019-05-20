c572af83bcbde553b8115f00d41a53d3d60f610a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-20 20:45:22 +0900

    Inventoryクラス追加

---
+++ Assets/HK/AutoAnt/Scripts/UserControllers/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UserControllers
+{
+    /// <summary>
+    /// 持ち物を保持する
+    /// </summary>
+    public sealed class Inventory
+    {
+        /// <summary>
+        /// アイテムリスト
+        /// </summary>
+        /// <remarks>
+        /// key = itemId
+        /// value = 個数
+        /// </remarks>
+        public IReadOnlyDictionary<int, int> Items => this.items;
+        private readonly Dictionary<int, int> items = new Dictionary<int, int>();
+
+        /// <summary>
+        /// アイテムを追加する
+        /// </summary>
+        public void AddItem(int itemId, int value)
+        {
+            if(this.items.ContainsKey(itemId))
+            {
+                this.items[itemId] += value;
+            }
+            else
+            {
+                this.items.Add(itemId, value);
+            }
+        }
+    }
+}
