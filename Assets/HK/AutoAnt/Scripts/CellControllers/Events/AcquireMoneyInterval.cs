bb7f820efba397337074e5be53d4105ade7f5f18
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-22 00:47:46 +0900

    AcquireMoneyInterval実装

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/AcquireMoneyInterval.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HK.AutoAnt.CellControllers.Gimmicks;
+using HK.AutoAnt.Systems;
+using HK.Framework.Text;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.CellControllers.Events
+{
+    /// <summary>
+    /// 一定間隔でお金を取得するセルイベント
+    /// </summary>
+    [CreateAssetMenu(menuName = "AutoAnt/Cell/Event/AcquireMoneyInterval")]
+    public sealed class AcquireMoneyInterval : CellEvent
+    {
+        [SerializeField]
+        private CellGimmickController gimmickPrefab;
+
+        /// <summary>
+        /// 取得するまでの時間
+        /// </summary>
+        [SerializeField]
+        private float intervalSeconds;
+
+        /// <summary>
+        /// 取得できる量
+        /// </summary>
+        [SerializeField]
+        private int amount;
+
+        public override CellGimmickController CreateGimmickController()
+        {
+            return Instantiate(this.gimmickPrefab);
+        }
+
+        public override void OnRegister(Cell owner)
+        {
+            Observable.Interval(TimeSpan.FromSeconds(this.intervalSeconds))
+                .TakeUntil(owner.ReleasedCellEventAsObservable())
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    GameSystem.Instance.User.Wallet.AddMoney(_this.amount);
+                })
+                .AddTo(owner);
+        }
+    }
+}
