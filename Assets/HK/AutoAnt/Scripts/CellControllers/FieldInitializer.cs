d293c82caf7943d9139307f512563460d7b891be
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-21 22:23:14 +0900

    FieldInitializer

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/FieldInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.CellControllers
+{
+    /// <summary>
+    /// フィールドを初期化するクラス
+    /// </summary>
+    [CreateAssetMenu(menuName = "AutoAnt/Cell/Field Initializer")]
+    public sealed class FieldInitializer : ScriptableObject
+    {
+        [SerializeField]
+        private List<Cell> cells = new List<Cell>();
+
+        [Serializable]
+        public class Cell
+        {
+            [SerializeField]
+            private Vector2Int id;
+
+            [SerializeField]
+            private CellUnitSpec unitSpec;
+        }
+    }
+}
