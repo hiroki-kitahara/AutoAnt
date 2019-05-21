64f0d307eb49eec584e052ff49f8eab5bce29654
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-21 23:25:23 +0900

    お金増えるイベント作ってみる

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/AcquireMoneyOnClick.cs
@@ -0,0 +1,36 @@
+﻿using HK.AutoAnt.Systems;
+using HK.Framework.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.CellControllers.ClickEvents
+{
+    /// <summary>
+    /// クリックでお金を取得するセルイベント
+    /// </summary>
+    [CreateAssetMenu(menuName = "AutoAnt/Cell/Event/AcquireMoneyOnClick")]
+    public sealed class AcquireMoneyOnClick : CellEvent
+    {
+        /// <summary>
+        /// 取得できる量
+        /// </summary>
+        [SerializeField]
+        private int amount;
+
+        /// <summary>
+        /// クリックされたらイベントを削除するか
+        /// </summary>
+        [SerializeField]
+        private bool onClickClearEvent;
+
+        public override void OnClick(Cell owner)
+        {
+            GameSystem.Instance.User.Wallet.AddMoney(this.amount);
+
+            if(this.onClickClearEvent)
+            {
+                owner.ClearEvent();
+            }
+        }
+    }
+}
