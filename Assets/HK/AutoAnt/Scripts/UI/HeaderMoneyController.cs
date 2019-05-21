caa4331945933cebcb5582cb5926668f7c3c687c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-21 22:16:40 +0900

    現在のお金をヘッダーUIに表示する

---
+++ Assets/HK/AutoAnt/Scripts/UI/HeaderMoneyController.cs
@@ -0,0 +1,31 @@
+﻿using HK.AutoAnt.Systems;
+using HK.Framework.Text;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// ヘッダーのお金UIを制御するクラス
+    /// </summary>
+    public sealed class HeaderMoneyController : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI value;
+
+        [SerializeField]
+        private StringAsset.Finder format;
+
+        void Start()
+        {
+            GameSystem.Instance.User.Wallet.MoneyAsObservable
+                .SubscribeWithState(this, (money, _this) =>
+                {
+                    _this.value.text = _this.format.Format(money.ToString());
+                })
+                .AddTo(this);
+        }
+    }
+}
