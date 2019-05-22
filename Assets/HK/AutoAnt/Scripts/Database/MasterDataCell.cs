3905b3db204feec02159f421632bef6fb9635417
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-22 21:53:14 +0900

    MasterDataCell実装

--- Assets/HK/AutoAnt/Scripts/Database/MasterDataCell.cs
+++ Assets/HK/AutoAnt/Scripts/Database/MasterDataCell.cs
@@ -1,14 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using HK.AutoAnt.CellControllers;
+using HK.AutoAnt.CellControllers.Events;
+using HK.AutoAnt.Constants;
+using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace HK.AutoAnt
+namespace HK.AutoAnt.Database
 {
     /// <summary>
     /// セルのマスターデータ
     /// </summary>
     [CreateAssetMenu(menuName = "AutoAnt/Database/Cell")]
-    public sealed class MasterDataCell : ScriptableObject
+    public sealed class MasterDataCell : MasterDataBase<MasterDataCell.Record>
     {
-        
+        [Serializable]
+        public class Record : IRecord
+        {
+            [SerializeField]
+            private int id = 0;
+            public int Id => this.id;
+
+            [SerializeField]
+            private CellType cellType = CellType.Grassland;
+            public CellType CellType => this.cellType;
+
+            [SerializeField]
+            private Cell prefab = null;
+            public Cell Prefab => this.prefab;
+
+            [SerializeField]
+            private List<CellEvent> clickEvents = new List<CellEvent>();
+            public List<CellEvent> ClickEvents => this.clickEvents;
+        }
     }
 }
