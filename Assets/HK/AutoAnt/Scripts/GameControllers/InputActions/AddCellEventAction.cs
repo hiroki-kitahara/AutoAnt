282675b572df975b894e051e51fc7e20f2cb13eb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-23 22:37:44 +0900

    セルにイベントを追加するアクションを追加

---
+++ Assets/HK/AutoAnt/Scripts/GameControllers/InputActions/AddCellEventAction.cs
@@ -0,0 +1,23 @@
+﻿using HK.AutoAnt.CameraControllers;
+using HK.AutoAnt.CellControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.GameControllers
+{
+    /// <summary>
+    /// セルにイベントを追加するアクション
+    /// </summary>
+    public sealed class AddCellEventAction : InputActions
+    {
+        public AddCellEventAction(CellEventGenerator eventGenerator)
+            : base(
+                null,
+                null,
+                new ClickToAddCellEvent(eventGenerator),
+                null
+            )
+        {
+        }
+    }
+}
