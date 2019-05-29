3d01e1901be2a7661000f213fee9c164f97823a8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-29 21:56:52 +0900

    とりあえずsizeを定義

--- Assets/HK/AutoAnt/Scripts/CellControllers/Events/CellEvent.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/CellEvent.cs
@@ -11,8 +11,24 @@ namespace HK.AutoAnt.CellControllers.Events
     {
         [SerializeField]
         private CellEventGenerateCondition condition;
-        
+
+        /// <summary>
+        /// 必要なセルのサイズ
+        /// </summary>
+        [SerializeField]
+        private Vector2Int size = Vector2Int.one;
+
         public abstract CellGimmickController CreateGimmickController();
+
+#if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+            var newSize = this.size;
+            newSize.x = newSize.x <= 1 ? 1 : newSize.x;
+            newSize.y = newSize.y <= 1 ? 1 : newSize.y;
+            this.size = newSize;
+        }
+#endif
 
         public bool CanGenerate(Cell owner)
         {
