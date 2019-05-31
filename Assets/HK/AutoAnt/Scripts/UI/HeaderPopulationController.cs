9f05e7944c57283d300c23d6827e90723956f37b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-01 00:04:39 +0900

    人口UIを実装

---
+++ Assets/HK/AutoAnt/Scripts/UI/HeaderPopulationController.cs
@@ -0,0 +1,31 @@
+﻿using HK.AutoAnt.Systems;
+using HK.Framework.Text;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// ヘッダーの人口UIを制御するクラス
+    /// </summary>
+    public sealed class HeaderPopulationController : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI value = null;
+
+        [SerializeField]
+        private StringAsset.Finder format = null;
+
+        void Start()
+        {
+            GameSystem.Instance.User.Town.PopulationAsObservable
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.value.text = _this.format.Format(x.ToString());
+                })
+                .AddTo(this);
+        }
+    }
+}
