bf8c85dca83d0b3033d6cf50dbfb489936290bc2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-05-31 23:48:53 +0900

    とりあえずTownを定義

---
+++ Assets/HK/AutoAnt/Scripts/UserControllers/Town.cs
@@ -0,0 +1,31 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UserControllers
+{
+    /// <summary>
+    /// 街のデータ
+    /// </summary>
+    public sealed class Town
+    {
+        /// <summary>
+        /// 人口
+        /// </summary>
+        public int Population => this.population.Value;
+
+        public IReactiveProperty<int> PopulationAsObservable => this.population;
+
+        private readonly ReactiveProperty<int> population = new ReactiveProperty<int>();
+
+        /// <summary>
+        /// 人口を加算する
+        /// </summary>
+        public void AddPopulation(int value)
+        {
+            this.population.Value += value;
+
+            Assert.IsTrue(this.Population >= 0);
+        }
+    }
+}
