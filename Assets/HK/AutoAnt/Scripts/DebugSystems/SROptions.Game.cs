63c4e09af2a49e24e7b2e865f202dbc056293248
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-03 01:10:39 +0900

    とりあえず建てる建設物IDを設定出来るように

---
+++ Assets/HK/AutoAnt/Scripts/DebugSystems/SROptions.Game.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using HK.AutoAnt.Systems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// ゲームに関するデバッグをまとめるクラス
+/// </summary>
+public partial class SROptions
+{
+    [Category("Game")]
+    [DisplayName("生成する建設物のID")]
+    public int ChangeCellEventGenerator
+    {
+        get
+        {
+            return GameSystem.Instance.CellManager.EventGenerator.RecordId;
+        }
+        set
+        {
+            GameSystem.Instance.CellManager.EventGenerator.RecordId = value;
+        }
+    }
+}
