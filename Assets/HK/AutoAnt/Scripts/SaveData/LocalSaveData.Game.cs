225f7b23245e4f5c7e8f05992b80485218bda9e5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-03 22:32:24 +0900

    SaveData関連のクラスを定義

---
+++ Assets/HK/AutoAnt/Scripts/SaveData/LocalSaveData.Game.cs
@@ -0,0 +1,18 @@
+﻿using HK.AutoAnt.CellControllers;
+using HK.AutoAnt.SaveData.Internal;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.SaveData
+{
+    /// <summary>
+    /// ゲーム関連のセーブデータ
+    /// </summary>
+    public static partial class LocalSaveData
+    {
+        public class GameSaveData
+        {
+            public readonly ISaveData<CellMapper> Mapper = new SaveData<CellMapper>("Game.CellMapper", new ES3Settings() { format = ES3.Format.JSON });
+        }
+    }
+}
