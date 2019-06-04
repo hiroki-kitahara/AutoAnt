f80f11d299c36108da0667dc4371fb31bbd21738
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-04 23:29:09 +0900

    シリアライズ可能なデータを定義

---
+++ Assets/HK/AutoAnt/Scripts/SaveData/Serializable/ISerializable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt
+{
+    /// <summary>
+    /// シリアライズ可能なインターフェイス
+    /// </summary>
+    public interface ISerializable<T>
+    {
+        /// <summary>
+        /// シリアライズする
+        /// </summary>
+        T Serialize();
+
+        /// <summary>
+        /// デシリアライズする
+        /// </summary>
+        void Deserialize(T target);
+    }
+}
---
+++ Assets/HK/AutoAnt/Scripts/SaveData/Serializable/SerializableCellMapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.SaveData.Serializables
+{
+    /// <summary>
+    /// <see cref="CellMapper"/>をシリアライズ可能にするクラス
+    /// </summary>
+    public sealed class SerializableCellMapper
+    {
+        public List<SerializableCell> Cells { get; set; } = new List<SerializableCell>();
+    }
+}
