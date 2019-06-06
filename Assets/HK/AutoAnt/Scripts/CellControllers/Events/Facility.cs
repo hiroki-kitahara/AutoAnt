a3ee67800a3ef017981e22d0ef83616087298dc8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-07 01:12:27 +0900

    Assets/FacilityLevelParameterから人気度を設定出来るように

--- Assets/HK/AutoAnt/Scripts/CellControllers/Events/Facility.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/Facility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HK.AutoAnt.CellControllers.Gimmicks;
+using HK.AutoAnt.Database;
 using HK.AutoAnt.Extensions;
 using HK.AutoAnt.GameControllers;
 using HK.AutoAnt.Systems;
@@ -44,17 +45,19 @@ namespace HK.AutoAnt.CellControllers.Events
 
         private GameSystem gameSystem;
 
+        private MasterDataFacilityLevelParameter.Record LevelParameter => this.gameSystem.MasterData.FacilityLevelParameter.Records.Get(this.Id, this.Level);
+
         public override void Initialize(Vector2Int position, GameSystem gameSystem)
         {
             base.Initialize(position, gameSystem);
             this.gameSystem = gameSystem;
-            gameSystem.User.Town.AddPopularity(this.PopularityAmount);
+            gameSystem.User.Town.AddPopularity(this.LevelParameter.Popularity);
         }
 
         public override void Remove(GameSystem gameSystem)
         {
             base.Remove(gameSystem);
-            gameSystem.User.Town.AddPopularity(-this.PopularityAmount);
+            gameSystem.User.Town.AddPopularity(-this.LevelParameter.Popularity);
         }
 
         public override void OnClick(Cell owner)
@@ -72,7 +75,15 @@ namespace HK.AutoAnt.CellControllers.Events
 
         public void LevelUp()
         {
+            // レベルアップ前の人気度を減算する
+            var oldPopularity = this.LevelParameter.Popularity;
+            this.gameSystem.User.Town.AddPopularity(-oldPopularity);
+
             this.LevelUp(this.gameSystem);
+
+            // レベルアップ後の人気度を加算する
+            var newPopularity = this.LevelParameter.Popularity;
+            this.gameSystem.User.Town.AddPopularity(newPopularity);
         }
     }
 }
