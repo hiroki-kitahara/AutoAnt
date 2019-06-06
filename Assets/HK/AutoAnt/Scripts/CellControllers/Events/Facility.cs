1a7ffb467f449501a5df5dfccedc25cb6079722f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-07 01:24:48 +0900

    ゲッターではなくキャッシュしておく

--- Assets/HK/AutoAnt/Scripts/CellControllers/Events/Facility.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/Facility.cs
@@ -30,19 +30,20 @@ namespace HK.AutoAnt.CellControllers.Events
 
         private GameSystem gameSystem;
 
-        private MasterDataFacilityLevelParameter.Record LevelParameter => this.gameSystem.MasterData.FacilityLevelParameter.Records.Get(this.Id, this.Level);
+        private MasterDataFacilityLevelParameter.Record levelParameter;
 
         public override void Initialize(Vector2Int position, GameSystem gameSystem)
         {
             base.Initialize(position, gameSystem);
             this.gameSystem = gameSystem;
-            gameSystem.User.Town.AddPopularity(this.LevelParameter.Popularity);
+            this.levelParameter = this.gameSystem.MasterData.FacilityLevelParameter.Records.Get(this.Id, this.Level);
+            gameSystem.User.Town.AddPopularity(this.levelParameter.Popularity);
         }
 
         public override void Remove(GameSystem gameSystem)
         {
             base.Remove(gameSystem);
-            gameSystem.User.Town.AddPopularity(-this.LevelParameter.Popularity);
+            gameSystem.User.Town.AddPopularity(-this.levelParameter.Popularity);
         }
 
         public override void OnClick(Cell owner)
@@ -61,13 +62,15 @@ namespace HK.AutoAnt.CellControllers.Events
         public void LevelUp()
         {
             // レベルアップ前の人気度を減算する
-            var oldPopularity = this.LevelParameter.Popularity;
+            var oldPopularity = this.levelParameter.Popularity;
             this.gameSystem.User.Town.AddPopularity(-oldPopularity);
 
             this.LevelUp(this.gameSystem);
 
+            this.levelParameter = this.gameSystem.MasterData.FacilityLevelParameter.Records.Get(this.Id, this.Level);
+
             // レベルアップ後の人気度を加算する
-            var newPopularity = this.LevelParameter.Popularity;
+            var newPopularity = this.levelParameter.Popularity;
             this.gameSystem.User.Town.AddPopularity(newPopularity);
         }
     }
