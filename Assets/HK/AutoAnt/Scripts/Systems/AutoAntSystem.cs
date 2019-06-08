1b20058c5332b6c1b4603515ac772ff1f95ad999
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-09 00:32:57 +0900

    AutoAntSystemにAudioSystemをもたせる

--- Assets/HK/AutoAnt/Scripts/Systems/AutoAntSystem.cs
+++ Assets/HK/AutoAnt/Scripts/Systems/AutoAntSystem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using HK.AutoAnt.AudioSystems;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace HK.AutoAnt.Systems
@@ -8,12 +9,23 @@ namespace HK.AutoAnt.Systems
     /// </summary>
     public class AutoAntSystem : MonoBehaviour
     {
+        [SerializeField]
+        private AudioSystem audioSystem;
+        public static AudioSystem Audio { get; private set; }
+        
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Setup()
         {
             var prefab = Resources.Load<GameObject>("AutoAntSystems");
+            Assert.IsNotNull(prefab);
+
             var instance = Instantiate(prefab);
             DontDestroyOnLoad(instance);
+
+            var system = instance.GetComponent<AutoAntSystem>();
+            Assert.IsNotNull(system);
+
+            Audio = system.audioSystem;
         }
     }
 }
