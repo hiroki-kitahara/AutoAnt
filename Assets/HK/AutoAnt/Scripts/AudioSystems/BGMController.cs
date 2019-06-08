fef2dc1b7eead574aafdd63dd45fdc4275fbdadc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-09 00:24:33 +0900

    AudioSystemとBGMController実装してみる

---
+++ Assets/HK/AutoAnt/Scripts/AudioSystems/BGMController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UniRx;
+using System;
+
+namespace HK.AutoAnt.AudioSystems
+{
+    /// <summary>
+    /// BGMを制御するクラス
+    /// </summary>
+    public sealed class BGMController : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        private readonly CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+        public void Play(AudioClip intro, AudioClip loop)
+        {
+            this.compositeDisposable.Clear();
+            this.audioSource.clip = intro;
+            this.audioSource.Play();
+            this.audioSource.loop = false;
+
+            Observable.Timer(TimeSpan.FromSeconds(intro.length))
+                .SubscribeWithState2(this, loop, (_, _this, _loop) =>
+                {
+                    _this.audioSource.clip = _loop;
+                    _this.audioSource.loop = true;
+                    _this.audioSource.Play();
+                })
+                .AddTo(this)
+                .AddTo(this.compositeDisposable);
+        }
+    }
+}
