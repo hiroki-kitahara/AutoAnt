19d63c07033cd3b928911a53829012bedc6ddf4a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-09 01:31:34 +0900

    SEを再生する機能を実装

---
+++ Assets/HK/AutoAnt/Scripts/AudioSystems/SEController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.AudioSystems
+{
+    /// <summary>
+    /// SEを制御するクラス
+    /// </summary>
+    public sealed class SEController : MonoBehaviour
+    {
+        [SerializeField]
+        private SEElement elementPrefab;
+
+        public void Play(AudioClip clip)
+        {
+            var element = this.elementPrefab.Rent();
+            element.transform.SetParent(this.transform);
+            element.Play(clip);
+            Observable.Timer(TimeSpan.FromSeconds(clip.length))
+                .SubscribeWithState(element, (_, _element) =>
+                {
+                    _element.Return();
+                })
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/HK/AutoAnt/Scripts/AudioSystems/SEElement.cs
@@ -0,0 +1,38 @@
+﻿using HK.Framework;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.AudioSystems
+{
+    /// <summary>
+    /// SEを再生する要素を持つクラス
+    /// </summary>
+    public sealed class SEElement : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        private static readonly ObjectPoolBundle<SEElement> pools = new ObjectPoolBundle<SEElement>();
+
+        private ObjectPool<SEElement> pool;
+
+        public SEElement Rent()
+        {
+            var pool = pools.Get(this);
+            var result = pool.Rent();
+            result.pool = pool;
+
+            return result;
+        }
+
+        public void Return()
+        {
+            this.pool.Return(this);
+        }
+
+        public void Play(AudioClip clip)
+        {
+            this.audioSource.PlayOneShot(clip);
+        }
+    }
+}
