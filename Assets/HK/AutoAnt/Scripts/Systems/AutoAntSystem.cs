e844fe637fb393e67755cafd50b15f8576d15ca6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-09 00:19:31 +0900

    アプリに常駐出来るシステムのAutoAntSystemを実装

---
+++ Assets/HK/AutoAnt/Scripts/Systems/AutoAntSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.Systems
+{
+    /// <summary>
+    /// このゲーム全体で利用するシステムを管理するクラス
+    /// </summary>
+    public class AutoAntSystem : MonoBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Setup()
+        {
+            var prefab = Resources.Load<GameObject>("AutoAntSystems");
+            var instance = Instantiate(prefab);
+            DontDestroyOnLoad(instance);
+        }
+    }
+}
