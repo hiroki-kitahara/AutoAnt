87059108215761b956ab53f90ad57e9eec1eea2b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-09 22:48:55 +0900

    不要なデータを削除

--- Assets/HK/AutoAnt/Scripts/Database/Runtime/Cell.cs
+++ Assets/HK/AutoAnt/Scripts/Database/Runtime/Cell.cs
@@ -33,39 +33,5 @@ namespace HK.AutoAnt.Database
             if (Records == null)
                 Records = new CellData[0];
         }
-
-        [SerializeField]
-        private ConstantData constants = null;
-        public ConstantData Constants => this.constants;
-
-        [Serializable]
-        public class ConstantData
-        {
-            [SerializeField]
-            private Vector3 scale = new Vector3(1.0f, 0.2f, 1.0f);
-            public Vector3 Scale => this.scale;
-
-            [SerializeField]
-            private Vector3 effectScale = Vector3.one;
-            public Vector3 EffectScale => this.effectScale;
-
-            [SerializeField]
-            private float interval = 1.5f;
-            public float Interval => this.interval;
-
-            /// <summary>
-            /// セル開拓時のSE
-            /// </summary>
-            [SerializeField]
-            private AudioClip developSE = null;
-            public AudioClip DevelopSE => this.developSE;
-
-            /// <summary>
-            /// 開拓するためのコスト係数
-            /// </summary>
-            [SerializeField]
-            private int developCost = 10;
-            public int DevelopCost => this.developCost;
-        }
     }
 }
