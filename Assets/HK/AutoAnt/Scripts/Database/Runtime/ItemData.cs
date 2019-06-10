e40cb5b4ac0da9bfa822bf69a2977cf9b04aa3a1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-10 23:09:45 +0900

    MasterDataItemのスプシ対応

---
+++ Assets/HK/AutoAnt/Scripts/Database/Runtime/ItemData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using HK.AutoAnt.Constants;
+
+namespace HK.AutoAnt.Database.SpreadSheetData
+{
+    ///
+    /// !!! Machine generated code !!!
+    /// !!! DO NOT CHANGE Tabs to Spaces !!!
+    ///
+    [System.Serializable]
+    public class ItemData : IRecord
+    {
+[SerializeField]
+int id;
+public int Id { get {return id; } set { id = value;} }
+
+[SerializeField]
+string name;
+public string Name { get {return name; } set { name = value;} }
+
+    }
+}
