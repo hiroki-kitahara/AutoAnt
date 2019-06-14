f27ffade5580ea5758f8b9b2a5bf2637943962af
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-15 00:06:13 +0900

    Townのパラメータのdouble対応中

--- Assets/HK/AutoAnt/Scripts/GameControllers/Calculator.cs
+++ Assets/HK/AutoAnt/Scripts/GameControllers/Calculator.cs
@@ -23,9 +23,9 @@ namespace HK.AutoAnt.GameControllers
         /// <param name="basePopulation">ベースの増加量</param>
         /// <param name="popularity">人気度</param>
         /// <param name="popularityRate">人気度の係数</param>
-        public static int AddPopulation(int basePopulation, int popularity, float popularityRate)
+        public static double AddPopulation(double basePopulation, double popularity, float popularityRate)
         {
-            return Mathf.FloorToInt(basePopulation * (popularity / popularityRate));
+            return System.Math.Floor(basePopulation * (popularity / popularityRate));
         }
     }
 }
