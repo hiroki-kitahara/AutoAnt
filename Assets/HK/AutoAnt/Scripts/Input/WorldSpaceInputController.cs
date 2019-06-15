f0971d458073d6b7ab0fa7ee45389dac57c4d65e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-15 21:37:20 +0900

    入力検知用のスクリプト作ってみる

---
+++ Assets/HK/AutoAnt/Scripts/Input/WorldSpaceInputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace HK.AutoAnt.InputControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class WorldSpaceInputController : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("PointerDown");
+        }
+    }
+}
