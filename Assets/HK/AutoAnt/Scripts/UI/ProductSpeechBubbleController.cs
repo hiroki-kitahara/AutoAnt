12a44965596f22187deba99330d450e905205fee
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-29 21:38:18 +0900

    とりあえずFacility生成されたらUIも生成されるように

---
+++ Assets/HK/AutoAnt/Scripts/UI/ProductSpeechBubbleController.cs
@@ -0,0 +1,29 @@
+﻿using HK.AutoAnt.CellControllers.Events;
+using HK.AutoAnt.Events;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// 生産物吹き出しUIを制御するクラス
+    /// </summary>
+    public sealed class ProductSpeechBubbleController : MonoBehaviour
+    {
+        [SerializeField]
+        private ProductSpeechBubbleElement elementPrefab;
+
+        void Awake()
+        {
+            Broker.Global.Receive<AddedCellEvent>()
+                .Where(x => x.CellEvent is Facility)
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    Instantiate(_this.elementPrefab, _this.transform, false).Initialize(x.CellEvent as Facility);
+                })
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/HK/AutoAnt/Scripts/UI/ProductSpeechBubbleElement.cs
@@ -0,0 +1,19 @@
+﻿using HK.AutoAnt.CellControllers.Events;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ProductSpeechBubbleElement : MonoBehaviour
+    {
+        public void Initialize(Facility facility)
+        {
+            Assert.IsNotNull(facility);
+
+            
+        }
+    }
+}
