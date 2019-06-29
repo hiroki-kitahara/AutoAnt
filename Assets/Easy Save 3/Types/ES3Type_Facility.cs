662b55ee7a891daff8f94b2e888466ac7ba87190
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-29 13:10:10 +0900

    Facilityが正しくセーブ出来ていなかった

--- Assets/Easy Save 3/Types/ES3Type_Facility.cs
+++ Assets/Easy Save 3/Types/ES3Type_Facility.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace ES3Types
 {
-	[ES3PropertiesAttribute("productTimer", "products", "size", "Level", "Origin", "name")]
+	[ES3PropertiesAttribute("Products", "size", "Level", "ProductTimer", "Origin", "name")]
 	public class ES3Type_Facility : ES3ScriptableObjectType
 	{
 		public static ES3Type Instance = null;
@@ -14,10 +14,10 @@ namespace ES3Types
 		{
 			var instance = (HK.AutoAnt.CellControllers.Events.Facility)obj;
 			
-			writer.WritePrivateField("productTimer", instance);
-			writer.WritePrivateField("products", instance);
+			writer.WriteProperty("Products", instance.Products);
 			writer.WritePrivateField("size", instance);
 			writer.WriteProperty("Level", instance.Level, ES3Type_int.Instance);
+			writer.WritePrivateProperty("ProductTimer", instance);
 			writer.WritePrivateProperty("Origin", instance);
 			writer.WriteProperty("name", instance.name, ES3Type_string.Instance);
 		}
@@ -30,18 +30,18 @@ namespace ES3Types
 				switch(propertyName)
 				{
 					
-					case "productTimer":
-					reader.SetPrivateField("productTimer", reader.Read<System.Single>(), instance);
-					break;
-					case "products":
-					reader.SetPrivateField("products", reader.Read<System.Collections.Generic.List<System.String>>(), instance);
-					break;
+					case "Products":
+						instance.Products = reader.Read<System.Collections.Generic.List<System.String>>();
+						break;
 					case "size":
 					reader.SetPrivateField("size", reader.Read<System.Int32>(), instance);
 					break;
 					case "Level":
 						instance.Level = reader.Read<System.Int32>(ES3Type_int.Instance);
 						break;
+					case "ProductTimer":
+					reader.SetPrivateProperty("ProductTimer", reader.Read<System.Single>(), instance);
+					break;
 					case "Origin":
 					reader.SetPrivateProperty("Origin", reader.Read<UnityEngine.Vector2Int>(), instance);
 					break;
