39db976fe00f5b34827dfe753503895a4d8262b6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-30 02:04:07 +0900

    FooterController作っておく

---
+++ Assets/HK/AutoAnt/Scripts/UI/FooterController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// フッターメニューを制御するクラス
+    /// </summary>
+    public sealed class FooterController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject root = null;
+
+        [SerializeField]
+        private GameObject selectBuilding = null;
+
+        void Awake()
+        {
+            this.ShowRoot();
+        }
+
+        public void ShowRoot()
+        {
+            this.AllHide();
+            this.root.SetActive(true);
+        }
+
+        public void ShowSelectBuilding()
+        {
+            this.AllHide();
+            this.selectBuilding.SetActive(true);
+        }
+
+        private void AllHide()
+        {
+            this.root.SetActive(false);
+            this.selectBuilding.SetActive(false);
+        }
+    }
+}
