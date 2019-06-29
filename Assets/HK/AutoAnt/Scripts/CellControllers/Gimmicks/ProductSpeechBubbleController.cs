cba4571c0b8c1a7e2e97f7fad477d65e20516dee
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-29 13:47:53 +0900

    ProductSpeechBubbleController実装

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Gimmicks/ProductSpeechBubbleController.cs
@@ -0,0 +1,29 @@
+﻿using HK.AutoAnt.CellControllers.Events;
+using UnityEngine;
+using UnityEngine.Assertions;
+using HK.AutoAnt.Events;
+using UniRx;
+
+namespace HK.AutoAnt.CellControllers.Gimmicks
+{
+    /// <summary>
+    /// 生産物吹き出しを制御するクラス
+    /// </summary>
+    public sealed class ProductSpeechBubbleController : MonoBehaviour, ICellEventGimmick
+    {
+        [SerializeField]
+        private GameObject target = null;
+
+        public void Attach(CellEvent cellEvent)
+        {
+            cellEvent.Broker.Receive<AddedFacilityProduct>()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    _this.target.SetActive(true);
+                })
+                .AddTo(this);
+
+            this.target.SetActive(false);
+        }
+    }
+}
