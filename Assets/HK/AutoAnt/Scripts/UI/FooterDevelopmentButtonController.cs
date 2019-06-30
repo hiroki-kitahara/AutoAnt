19811f612330579da2bce8a1e2cb2833e9ec5044
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-06-30 12:36:35 +0900

    とりあえずFooterDevelopmentButtonController作る

---
+++ Assets/HK/AutoAnt/Scripts/UI/FooterDevelopmentButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using HK.AutoAnt.Extensions;
+using HK.AutoAnt.Systems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// フッターメニューの開拓ボタンを制御するクラス
+    /// </summary>
+    public sealed class FooterDevelopmentButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button = null;
+
+        [SerializeField]
+        private FooterController footerController = null;
+
+        void Awake()
+        {
+            this.button.OnClickAsObservable()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                })
+                .AddTo(this);
+        }
+    }
+}
