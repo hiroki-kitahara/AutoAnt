f6e482971f01b1065f8aa57715f555aedc798eb1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-01 22:21:41 +0900

    とりあえず道路プレハブ生成されるように

--- Assets/HK/AutoAnt/Scripts/CellControllers/Gimmicks/RoadModelSwitcher.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Gimmicks/RoadModelSwitcher.cs
@@ -1,4 +1,5 @@
 ﻿using HK.AutoAnt.CellControllers.Events;
+using HK.Framework;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -9,8 +10,30 @@ namespace HK.AutoAnt.CellControllers.Gimmicks
     /// </summary>
     public sealed class RoadModelSwitcher : MonoBehaviour, ICellEventGimmick
     {
+        [SerializeField]
+        private PoolableObject straight;
+
+        [SerializeField]
+        private PoolableObject turn;
+
+        [SerializeField]
+        private PoolableObject junctionT;
+
+        [SerializeField]
+        private PoolableObject crossroads;
+
+        private PoolableObject currentObject;
+
+        private ObjectPool<PoolableObject> currentPool;
+
+        private static readonly ObjectPoolBundle<PoolableObject> pools = new ObjectPoolBundle<PoolableObject>();
+
         public void Attach(CellEvent cellEvent)
         {
+            this.currentPool = pools.Get(this.straight);
+            this.currentObject = this.currentPool.Rent();
+            this.currentObject.transform.SetParent(this.transform);
+            this.currentObject.transform.localPosition = Vector3.zero;
         }
     }
 }
