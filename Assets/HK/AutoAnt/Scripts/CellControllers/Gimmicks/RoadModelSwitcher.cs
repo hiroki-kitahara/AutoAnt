8602120d58afad97bc652d3296616048fff83ca9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-01 21:23:05 +0900

    とりあえずRoadModelSwitcher定義

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Gimmicks/RoadModelSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using HK.AutoAnt.CellControllers.Events;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.CellControllers.Gimmicks
+{
+    /// <summary>
+    /// 道路のモデルを切り替えるクラス
+    /// </summary>
+    public sealed class RoadModelSwitcher : MonoBehaviour, ICellEventGimmick
+    {
+        public void Attach(CellEvent cellEvent)
+        {
+        }
+    }
+}
