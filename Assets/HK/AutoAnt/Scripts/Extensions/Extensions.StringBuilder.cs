e44a05e0f3b4b46b0f475c7057e73ef116aa8659
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-04 21:10:31 +0900

    カラーコード付きの文字列を返せる拡張関数を実装

---
+++ Assets/HK/AutoAnt/Scripts/Extensions/Extensions.StringBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HK.AutoAnt.Extensions
+{
+    /// <summary>
+    /// <see cref="StringBuilder"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// colorタグ付きの文字列をアペンドする
+        /// </summary>
+        public static StringBuilder AppendColorCode(this StringBuilder self, Color color, string value)
+        {
+            return self
+                .Append("<color=#")
+                .Append(ColorUtility.ToHtmlStringRGBA(color))
+                .Append(">")
+                .Append(value)
+                .Append("</color>");
+        }
+    }
+}
