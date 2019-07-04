2704c21a3488a233aa8609faddc171672ba4c6dc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-04 22:05:53 +0900

    インプット切り替えデバッグボタンを削除

--- Assets/HK/AutoAnt/Scripts/Systems/Constants.cs
+++ Assets/HK/AutoAnt/Scripts/Systems/Constants.cs
@@ -19,29 +19,6 @@ namespace HK.AutoAnt.Constants
         Grassland,
     }
 
-    public enum InputMode
-    {
-        /// <summary>
-        /// クリックモード
-        /// </summary>
-        ClickMode,
-
-        /// <summary>
-        /// 建築モード
-        /// </summary>
-        BuildMode,
-
-        /// <summary>
-        /// 解体モード
-        /// </summary>
-        DismantleMode,
-
-        /// <summary>
-        /// 開拓モード
-        /// </summary>
-        ExploringMode,
-    }
-
     public enum CellEventCategory
     {
         /// <summary>
