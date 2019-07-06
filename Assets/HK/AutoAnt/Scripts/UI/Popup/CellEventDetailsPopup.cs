6fd80f223f87f738f97be94c306e94c0f668177e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-06 21:44:40 +0900

    CellEventDetailsPopupもアニメーションするように

--- Assets/HK/AutoAnt/Scripts/UI/Popup/CellEventDetailsPopup.cs
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/CellEventDetailsPopup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using HK.AutoAnt.CellControllers.Events;
+using HK.AutoAnt.Extensions;
 using HK.Framework.Text;
 using TMPro;
 using UnityEngine;
@@ -12,7 +14,7 @@ namespace HK.AutoAnt.UI
     /// <summary>
     /// セルイベントの詳細を表示するポップアップ
     /// </summary>
-    public sealed class CellEventDetailsPopup : Popup
+    public sealed class CellEventDetailsPopup : Popup, ITweenPopup
     {
         [SerializeField]
         private TextMeshProUGUI title = null;
@@ -38,6 +40,9 @@ namespace HK.AutoAnt.UI
 
         [SerializeField]
         private CellEventDetailsPopupProperty propertyPrefab = null;
+
+        [SerializeField]
+        private GameObject tweenAnimationHolder = null;
 
         [SerializeField]
         private StringAsset.Finder cellEventNameAndLevelFormat = null;
@@ -79,17 +84,35 @@ namespace HK.AutoAnt.UI
         private Color notEnoughLevelUpCostColor = Color.white;
         public Color NotEnoughLevelUpCostColor => notEnoughLevelUpCostColor;
 
+        private DOTweenAnimation[] tweenAnimations;
+        DOTweenAnimation[] ITweenPopup.TweenAnimations => this.tweenAnimations;
+
         private readonly List<CellEventDetailsPopupProperty> properties = new List<CellEventDetailsPopupProperty>();
 
         private readonly List<CellEventDetailsPopupProperty> levelUpCosts = new List<CellEventDetailsPopupProperty>();
 
         public CellEvent SelectCellEvent { get; private set; }
 
+        void Awake()
+        {
+            this.tweenAnimations = this.tweenAnimationHolder.GetComponentsInChildren<DOTweenAnimation>();
+        }
+
         public void Initialize(CellEvent cellEvent)
         {
             this.SelectCellEvent = cellEvent;
             this.SelectCellEvent.AttachDetailsPopup(this);
             this.UpdateElement();
+        }
+
+        public override void Open()
+        {
+            this.StartTweeningOpen();
+        }
+
+        public override void Close()
+        {
+            this.StartTweeningClose();
         }
 
         public void UpdateElement()
