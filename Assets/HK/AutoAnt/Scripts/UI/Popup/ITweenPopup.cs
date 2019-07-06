7343217dd01101154035fe0b8e8eff3055ff1811
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-06 20:17:17 +0900

    ITweenPopup実装

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/ITweenPopup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using DG.Tweening;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// ツイーンで表示と非表示を行えるポップアップのインターフェイス
+    /// </summary>
+    public interface ITweenPopup
+    {
+        /// <summary>
+        /// アニメーションリスト
+        /// </summary>
+        DOTweenAnimation[] TweenAnimations { get; }
+    }
+}
