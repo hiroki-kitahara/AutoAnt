8e4436151eab300a3055ec3a3fc17e0bf688bcd7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-06 21:54:15 +0900

    TweenPopup作ったほうが楽そう

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/TweenPopup.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using HK.AutoAnt.Extensions;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// 表示非表示をアニメーションするポップアップ
+    /// </summary>
+    public abstract class TweenPopup : Popup, ITweenPopup
+    {
+        [SerializeField]
+        private GameObject tweenAnimationHolder = null;
+
+        private DOTweenAnimation[] tweenAnimations;
+        DOTweenAnimation[] ITweenPopup.TweenAnimations => throw new System.NotImplementedException();
+
+        protected virtual void Awake()
+        {
+            this.tweenAnimations = this.tweenAnimationHolder.GetComponentsInChildren<DOTweenAnimation>();
+        }
+
+        public override void Open()
+        {
+            this.StartTweeningOpen();
+        }
+
+        public override void Close()
+        {
+            this.StartTweeningClose();
+        }
+    }
+}
