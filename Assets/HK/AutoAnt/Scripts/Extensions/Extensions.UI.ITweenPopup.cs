2e3de7fc4072b8ab46cae4753dd9b7169680f43d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-06 20:20:30 +0900

    ITweenPopupの拡張関数追加

---
+++ Assets/HK/AutoAnt/Scripts/Extensions/Extensions.UI.ITweenPopup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HK.AutoAnt.Events;
+using HK.AutoAnt.UI;
+using UniRx;
+
+namespace HK.AutoAnt.Extensions
+{
+    /// <summary>
+    /// <see cref="ITweenPopup"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// ツイーンしながら表示を開始する
+        /// </summary>
+        public static void StartTweeningOpen(this ITweenPopup self)
+        {
+        }
+
+        /// <summary>
+        /// ツイーンしながら非表示を開始する
+        /// </summary>
+        public static void StartTweeningClose(this ITweenPopup self)
+        {
+        }
+    }
+}
