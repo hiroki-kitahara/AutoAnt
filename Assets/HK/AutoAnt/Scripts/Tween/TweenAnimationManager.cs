c761c4a0003aacb4dbdd4a221e0397736ac02ed2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-09 19:26:35 +0900

    TweenAnimationManagerでTweenを管理する

---
+++ Assets/HK/AutoAnt/Scripts/Tween/TweenAnimationManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using DG.Tweening;
+
+namespace HK.AutoAnt
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class TweenAnimationManager : MonoBehaviour
+    {
+        public void Attach(GameObject target)
+        {
+            var animations = this.GetComponents<DOTweenAnimation>();
+            foreach(var a in animations)
+            {
+                a.targetGO = target;
+            }
+        }
+    }
+}
