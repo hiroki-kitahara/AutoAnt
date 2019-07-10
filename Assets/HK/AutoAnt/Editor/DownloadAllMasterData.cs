2e7c010d3587c43ece00ae4aca48d7ceb4c2ca08
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-10 21:18:23 +0900

    とりあえずDownloadAllMasterData定義しておく

---
+++ Assets/HK/AutoAnt/Editor/DownloadAllMasterData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using HK.AutoAnt.Database;
+
+namespace HK.AutoAnt.Editor
+{
+    /// <summary>
+    /// マスターデータを全てダウンロードするエディタ拡張
+    /// </summary>
+    public sealed class DownloadAllMasterData
+    {
+        [MenuItem("AutoAnt/MasterData/Download All")]
+        private static void DownloadAll()
+        {
+            var masterData = AssetDatabase.LoadAssetAtPath<MasterData>("Assets/HK/AutoAnt/DataSources/Database/MasterData/MasterData.asset");
+            
+        }
+    }
+}
