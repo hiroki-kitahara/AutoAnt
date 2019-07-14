a4d2bb72b8dcb4f6a40b174f33c0a35a0e486ed4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-15 01:02:54 +0900

    FooterElementを定義

---
+++ Assets/HK/AutoAnt/Scripts/UI/FooterElement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// フッターの要素を制御する抽象クラス
+    /// </summary>
+    public abstract class FooterElement : MonoBehaviour, IFooterElement
+    {
+        public abstract void Open();
+
+        public abstract void Close();
+    }
+}
