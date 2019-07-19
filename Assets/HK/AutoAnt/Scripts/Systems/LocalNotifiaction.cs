57d8fe8ff1f173bc9f9db6674210b7fb86509a68
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-19 20:22:26 +0900

    iOS対応

--- Assets/HK/AutoAnt/Scripts/Systems/LocalNotifiaction.cs
+++ Assets/HK/AutoAnt/Scripts/Systems/LocalNotifiaction.cs
@@ -1,6 +1,11 @@
-﻿using Unity.Notifications.Android;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+using Unity.Notifications.Android;
+#elif UNITY_IOS && !UNITY_EDITOR
+using Unity.Notifications.iOS;
+#endif
 
 namespace HK.AutoAnt.Systems
 {
@@ -34,16 +39,16 @@ namespace HK.AutoAnt.Systems
 #endif
         }
 
-        public void Register(string title, string message, double delaySeconds)
+        public void Register(string title, string message, int afterSeconds)
         {
 #if UNITY_ANDROID && !UNITY_EDITOR
-            this.Register(title, message, delaySeconds, DefaultSmallIcon, DefaultLargeIcon);
+            this.Register(title, message, afterSeconds, DefaultSmallIcon, DefaultLargeIcon);
 #elif UNITY_IOS && !UNITY_EDITOR
-            this.Register(title, message, delaySeconds, "", "");
+            this.Register(title, message, afterSeconds, "", "");
 #endif
         }
 
-        public void Register(string title, string message, double delaySeconds, string smallIcon, string largeIcon)
+        public void Register(string title, string message, int afterSeconds, string smallIcon, string largeIcon)
         {
 #if UNITY_ANDROID && !UNITY_EDITOR
             AndroidNotificationCenter.SendNotification(
@@ -53,13 +58,24 @@ namespace HK.AutoAnt.Systems
                     Text = message,
                     SmallIcon = smallIcon,
                     LargeIcon = largeIcon,
-                    FireTime = System.DateTime.Now.AddSeconds(delaySeconds),
+                    FireTime = System.DateTime.Now.AddSeconds(afterSeconds),
                     Number = 1
                 },
                 DefaultChannelId
             );
 #elif UNITY_IOS && !UNITY_EDITOR
-
+            iOSNotificationCenter.ScheduleNotification(new iOSNotification
+            {
+                Title = title,
+                Body = message,
+                ShowInForeground = true,
+                Badge = 1,
+                Trigger = new iOSNotificationTimeIntervalTrigger
+                {
+                    TimeInterval = new System.TimeSpan(0, 0, afterSeconds),
+                    Repeats = false
+                }
+            });
 #endif
         }
     }
