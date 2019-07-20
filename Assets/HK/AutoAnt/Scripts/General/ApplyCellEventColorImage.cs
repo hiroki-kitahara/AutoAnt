908b2ac82def22ab479d0c0b0956bfcbdd7f9570
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-20 18:49:50 +0900

    ApplyCellEventColorImage実装

---
+++ Assets/HK/AutoAnt/Scripts/General/ApplyCellEventColorImage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using HK.AutoAnt.Constants;
+using UnityEngine.UI;
+using HK.AutoAnt.Systems;
+
+namespace HK.AutoAnt
+{
+    /// <summary>
+    /// <see cref="ConstantsColor.CellEventColors"/>を<see cref="Image"/>に適用する
+    /// </summary>
+    public sealed class ApplyCellEventColorImage : MonoBehaviour
+    {
+        [SerializeField]
+        private Image target;
+
+        [SerializeField]
+        private CellEventCategory category;
+
+        void Start()
+        {
+            this.target.color = GameSystem.Instance.Constants.Color.CellEvent.Get(this.category);
+        }
+    }
+}
