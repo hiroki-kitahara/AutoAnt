5f17820d06aeb3dd68ff343d25bff82360f0afaa
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-20 13:01:08 +0900

    とりあえずUnlockCellEventPopupを実装

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/UnlockCellEventPopup.cs
@@ -0,0 +1,19 @@
+﻿using HK.AutoAnt.CellControllers.Events;
+using TMPro;
+using UnityEngine;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// アンロックされたセルイベントを表示するポップアップ
+    /// </summary>
+    public sealed class UnlockCellEventPopup : TweenPopup
+    {
+        [SerializeField]
+        private TextMeshProUGUI message = null;
+
+        public void Initialize(CellEvent cellEvent)
+        {
+        }
+    }
+}
