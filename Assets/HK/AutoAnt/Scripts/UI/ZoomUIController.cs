c9170a25cf5257137536674205e4463b6e0e7cb1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-21 00:03:47 +0900

    とりあえずZoomUIController作る

---
+++ Assets/HK/AutoAnt/Scripts/UI/ZoomUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UniRx;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// ズームUIを制御するクラス
+    /// </summary>
+    public sealed class ZoomUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider slider = null;
+
+        void Start()
+        {
+            this.slider.OnValueChangedAsObservable()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    Debug.Log(x);
+                })
+                .AddTo(this);
+        }
+    }
+}
