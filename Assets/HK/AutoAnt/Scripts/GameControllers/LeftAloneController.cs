69d9abf0be5391dc6d7b43ffcf5914633b16ed06
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-21 23:31:18 +0900

    LeftAloneControllerを定義

---
+++ Assets/HK/AutoAnt/Scripts/GameControllers/LeftAloneController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.GameControllers
+{
+    /// <summary>
+    /// ゲームを放置した際の処理を行うクラス
+    /// </summary>
+    public sealed class LeftAloneController : MonoBehaviour
+    {
+        
+    }
+}
