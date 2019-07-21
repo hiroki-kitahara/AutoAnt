d768fb9647f5c54b465054f937bcde475acec5c7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-21 13:44:02 +0900

    GameResume追加

---
+++ Assets/HK/AutoAnt/Scripts/Events/GameResume.cs
@@ -0,0 +1,14 @@
+﻿using HK.AutoAnt.Systems;
+using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.Events
+{
+    /// <summary>
+    /// ゲームが復帰した際のイベント
+    /// </summary>
+    public sealed class GameResume : Message<GameResume>
+    {
+    }
+}
