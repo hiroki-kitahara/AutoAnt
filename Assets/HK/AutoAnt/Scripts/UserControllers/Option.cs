a5ab15cf5032e67767665e4ef38208cb58a1bf72
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-22 21:16:57 +0900

    Option実装

---
+++ Assets/HK/AutoAnt/Scripts/UserControllers/Option.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UserControllers
+{
+    /// <summary>
+    /// ユーザーデータのオプションオプションを管理するクラス
+    /// </summary>
+    [Serializable]
+    public sealed class Option
+    {
+        public float BGMVolume = 0.5f;
+
+        public float SEVolume = 0.5f;
+    }
+}
