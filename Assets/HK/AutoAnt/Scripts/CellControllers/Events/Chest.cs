6d8b23dd81ff3108794a5473141e81772fe69400
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-30 00:19:44 +0900

    とりあえずChestクラス定義

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/Chest.cs
@@ -0,0 +1,51 @@
+﻿using HK.AutoAnt.Database;
+using HK.AutoAnt.Extensions;
+using HK.AutoAnt.GameControllers;
+using HK.AutoAnt.Systems;
+using HK.AutoAnt.UserControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+using HK.AutoAnt.EffectSystems;
+using HK.Framework.EventSystems;
+using HK.AutoAnt.Events;
+using HK.AutoAnt.UI;
+using UniRx.Triggers;
+using UniRx;
+
+namespace HK.AutoAnt.CellControllers.Events
+{
+    /// <summary>
+    /// 貯蔵のセルイベント
+    /// </summary>
+    /// <remarks>
+    /// - やっていること
+    ///     - アイテムを貯蔵出来る
+    /// </remarks>
+    [CreateAssetMenu(menuName = "AutoAnt/Cell/Event/Chest")]
+    public sealed class Chest : CellEvent
+    {
+        public override void AttachDetailsPopup(CellEventDetailsPopup popup)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void AttachFooterSelectCellEvent(FooterSelectBuildingController controller)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnClick(Cell owner)
+        {
+        }
+
+        public override void UpdateDetailsPopup(CellEventDetailsPopup popup)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void UpdateFooterSelectCellEvent(FooterSelectBuildingController controller)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
