1dec091ae3daead625ed5135d0ee8b360e54b857
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-30 00:55:44 +0900

    IOpenCellEventDetailsPopupを定義

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/IOpenCellEventDetailsPopup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using HK.AutoAnt.UI;
+
+namespace HK.AutoAnt.CellControllers.Events
+{
+    /// <summary>
+    /// <see cref="CellEventDetailsPopup"/>を開くことが出来るセルイベントのインターフェイス
+    /// </summary>
+    public interface IOpenCellEventDetailsPopup
+    {
+        void Attach(CellEventDetailsPopup popup);
+
+        void Update(CellEventDetailsPopup popup);
+    }
+}
