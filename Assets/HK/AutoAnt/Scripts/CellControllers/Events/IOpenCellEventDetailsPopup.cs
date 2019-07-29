1c92588d44346eb062fae176c69c3a4a1a89af75
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-07-30 01:00:51 +0900

    思いやりコメント

--- Assets/HK/AutoAnt/Scripts/CellControllers/Events/IOpenCellEventDetailsPopup.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/IOpenCellEventDetailsPopup.cs
@@ -9,8 +9,14 @@ namespace HK.AutoAnt.CellControllers.Events
     /// </summary>
     public interface IOpenCellEventDetailsPopup
     {
+        /// <summary>
+        /// アタッチする
+        /// </summary>
         void Attach(CellEventDetailsPopup popup);
 
+        /// <summary>
+        /// ポップアップを更新する
+        /// </summary>
         void Update(CellEventDetailsPopup popup);
     }
 }
