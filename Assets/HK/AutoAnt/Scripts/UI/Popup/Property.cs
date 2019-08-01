e2a3a035a8de71b385301d10a0536b21a66ea8e1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-01 23:59:41 +0900

    PrefixとValueを持つUIElementを実装

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/Property.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace HK.AutoAnt.UI.Elements
+{
+    /// <summary>
+    /// PrefixとValueを表示するUIElement
+    /// </summary>
+    public sealed class Property : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI prefix = null;
+        public TextMeshProUGUI Prefix => this.prefix;
+
+        [SerializeField]
+        private TextMeshProUGUI value = null;
+        public TextMeshProUGUI Value => this.value;
+
+        private Action<Property> updateAction = null;
+
+        public void Initialize(Action<Property> updateAction)
+        {
+            this.updateAction = updateAction;
+            this.updateAction(this);
+        }
+
+        public void UpdateProperty()
+        {
+            this.updateAction(this);
+        }
+    }
+}
