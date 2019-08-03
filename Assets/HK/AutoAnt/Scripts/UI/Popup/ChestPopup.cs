8dfd577f4b9b6cc245d4c4cedbbda05c6e2b3cb9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-03 22:31:17 +0900

    Popup.Chestを制御出来るように

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/ChestPopup.cs
@@ -0,0 +1,21 @@
+﻿using HK.AutoAnt.UI.Elements;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// 貯蔵ポップアップを制御するクラス
+    /// </summary>
+    public sealed class ChestPopup : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI title = null;
+        public TextMeshProUGUI Title => this.title;
+
+        [SerializeField]
+        private GridList gridList = null;
+        public GridList GridList => this.gridList;
+    }
+}
