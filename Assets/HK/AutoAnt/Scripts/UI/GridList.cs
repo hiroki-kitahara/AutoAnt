1f8e358effdac932ae5ea185df051be66c752ff9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-03 17:46:42 +0900

    GridListを制御出来るように

---
+++ Assets/HK/AutoAnt/Scripts/UI/GridList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.UI.Elements
+{
+    /// <summary>
+    /// グリッドリストを制御するクラス
+    /// </summary>
+    public sealed class GridList : MonoBehaviour
+    {
+        [SerializeField]
+        private GridListElement elementPrefab = null;
+
+        [SerializeField]
+        private Transform parent = null;
+
+        public void SetData<T>(List<T> list, Action<int, T, GridListElement> setAction)
+        {
+            
+        }
+    }
+}
