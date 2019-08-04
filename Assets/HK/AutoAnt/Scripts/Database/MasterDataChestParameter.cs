ec81427788a72270fdd91439a71787affac3e232
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-04 13:47:39 +0900

    MasterDataChestParameter実装

---
+++ Assets/HK/AutoAnt/Scripts/Database/MasterDataChestParameter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+#endif
+
+namespace HK.AutoAnt.Database
+{
+    /// <summary>
+    /// チェストのマスターデータ
+    /// </summary>
+    [CreateAssetMenu(menuName = "AutoAnt/Database/ChestParameter")]
+    public sealed class MasterDataChestParameter : MasterDataBase<MasterDataChestParameter.Record>
+    {
+        [Serializable]
+        public class Record : IRecord
+        {
+            [SerializeField]
+            private int id = 0;
+            public int Id => this.id;
+
+            /// <summary>
+            /// アイテムを貯蔵出来る容量
+            /// </summary>
+            [SerializeField]
+            private int capacity = 1;
+            public int Capacity => this.capacity;
+
+#if UNITY_EDITOR
+#endif
+        }
+    }
+}
