146f533e880e1d1f5894eaaab6ba3f6510a6c28f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-04 14:16:36 +0900

    IChest実装

---
+++ Assets/HK/AutoAnt/Scripts/CellControllers/Events/IChest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HK.AutoAnt.GameControllers;
+
+namespace HK.AutoAnt.CellControllers.Events
+{
+    /// <summary>
+    /// 貯蔵可能なセルイベントのインターフェイス
+    /// </summary>
+    public interface IChest
+    {
+        /// <summary>
+        /// 貯蔵しているアイテム
+        /// </summary>
+        List<StackedItem> Items { get; }
+        
+        /// <summary>
+        /// <paramref name="itemId"/>を追加する
+        /// </summary>
+        /// <remarks>
+        /// 既に<paramref name="itemId"/>を持っている場合はスタックされます
+        /// </remarks>
+        void Add(int itemId);
+
+        /// <summary>
+        /// <paramref name="listId"/>に紐づくアイテムを取り出す
+        /// </summary>
+        void PickOut(int listId);
+    }
+}
