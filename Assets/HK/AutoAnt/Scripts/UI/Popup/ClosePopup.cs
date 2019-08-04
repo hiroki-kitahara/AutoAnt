c74d00ce2af79d03b53f6ea1e3a1a2d71a45c4f6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-04 13:23:54 +0900

    ClosePopup実装

---
+++ Assets/HK/AutoAnt/Scripts/UI/Popup/ClosePopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HK.AutoAnt.UI
+{
+    /// <summary>
+    /// 閉じるポップアップを制御するクラス
+    /// </summary>
+    public sealed class ClosePopup : TweenPopup
+    {
+        [SerializeField]
+        private Button button = null;
+        public Button Button => this.button;
+    }
+}
