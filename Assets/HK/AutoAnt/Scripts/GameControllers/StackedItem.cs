8f58e4695312493e92e3051f53dbe7b052c0fa04
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-04 14:16:11 +0900

    StackedItem実装

---
+++ Assets/HK/AutoAnt/Scripts/GameControllers/StackedItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HK.AutoAnt.GameControllers
+{
+    /// <summary>
+    /// スタックされたアイテム
+    /// </summary>
+    [Serializable]
+    public sealed class StackedItem
+    {
+        /// <summary>
+        /// アイテムのID
+        /// </summary>
+        public int ItemId { get; private set; }
+
+        /// <summary>
+        /// アイテムの量
+        /// </summary>
+        public int Amount { get; private set; }
+    }
+}
