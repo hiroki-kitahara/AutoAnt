b279c7a21e70f8cec05a5cfa6c3b07c26c1d1887
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-08-05 23:03:48 +0900

    GridListElementを更新出来るように

--- Assets/HK/AutoAnt/Scripts/UI/GridList.cs
+++ Assets/HK/AutoAnt/Scripts/UI/GridList.cs
@@ -17,15 +17,37 @@ namespace HK.AutoAnt.UI.Elements
         [SerializeField]
         private Transform elementParent = null;
 
+        private GridListElement[] elements = null;
+
         public void SetData<T>(IEnumerable<T> list, Action<int, T, GridListElement> setAction)
         {
+            this.NewElements(list.Count());
             var array = list.ToArray();
             for (var i = 0; i < array.Length; i++)
             {
                 var element = this.elementPrefab.Rent();
+                this.elements[i] = element;
                 element.transform.SetParent(this.elementParent);
                 setAction(i, array[i], element);
             }
+        }
+
+        public void UpdateData<T>(IEnumerable<T> list, int targetIndex, Action<int, T, GridListElement> updateAction)
+        {
+            updateAction(targetIndex, list.ToArray()[targetIndex], this.elements[targetIndex]);
+        }
+
+        private void NewElements(int length)
+        {
+            if(this.elements != null)
+            {
+                foreach (var e in this.elements)
+                {
+                    e.Return();
+                }
+            }
+
+            this.elements = new GridListElement[length];
         }
     }
 }
