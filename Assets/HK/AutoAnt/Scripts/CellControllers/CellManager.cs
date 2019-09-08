cb06d9b46d18ea2e0385cc922e81c65d1b26f0a6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-08 18:11:17 +0900

    不要なアセットを削除

--- Assets/HK/AutoAnt/Scripts/CellControllers/CellManager.cs
+++ Assets/HK/AutoAnt/Scripts/CellControllers/CellManager.cs
@@ -19,9 +19,6 @@ namespace HK.AutoAnt.CellControllers
         [SerializeField]
         private Transform parent = null;
 
-        [SerializeField]
-        private FieldInitializer fieldInitializer = null;
-
         public CellMapper Mapper { get; private set; }
 
         public CellGenerator CellGenerator { get; private set; }
