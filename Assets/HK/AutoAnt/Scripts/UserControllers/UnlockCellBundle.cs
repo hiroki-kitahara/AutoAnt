b9cdbbc17210851bfce9886329895bb9b63b9375
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-10 21:29:15 +0900

    思いやりコメント

--- Assets/HK/AutoAnt/Scripts/UserControllers/UnlockCellBundle.cs
+++ Assets/HK/AutoAnt/Scripts/UserControllers/UnlockCellBundle.cs
@@ -12,12 +12,21 @@ namespace HK.AutoAnt.UserControllers
     [Serializable]
     public sealed class UnlockCellBundle
     {
+        /// <summary>
+        /// 次にアンロックする人口数
+        /// </summary>
         public int NextPopulation => this.nextPopulation;
         private int nextPopulation = int.MinValue;
 
+        /// <summary>
+        /// 次にアンロックする人口数を設定する
+        /// </summary>
         public void SetNextPopulation(MasterDataUnlockCellBundle masterData)
         {
+            // 現状の次の人口数より大きい値のレコードを取得する
             var targets = masterData.Records.Where(x => this.nextPopulation < x.NeedPopulation).ToList();
+
+            // 取得したレコードから最小値のレコードを対象とする
             var id = -1;
             var min = int.MaxValue;
             for (var i = 0; i < targets.Count; i++)
