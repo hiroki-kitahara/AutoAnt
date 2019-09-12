8c397a515b6dd6be0168794e218c230ace45e6aa
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-12 23:21:00 +0900

    とりあえずWindow出してみる

---
+++ Assets/HK/AutoAnt/Editor/CellBundleGenerator/CellBundleGeneratorWindow.cs
@@ -0,0 +1,43 @@
+﻿using HK.AutoAnt.Database;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.AutoAnt.Editor
+{
+    /// <summary>
+    /// <see cref="MasterDataCellBundle"/>を生成するツールウィンドウ
+    /// </summary>
+    public sealed class CellBundleGeneratorWindow : EditorWindow
+    {
+        [SerializeField]
+        private MasterDataCellBundle target = null;
+        
+        [MenuItem("AutoAnt/Tool/CellBundleGenerator")]
+        private static void CreateWindow()
+        {
+            var window = EditorWindow.GetWindow<CellBundleGeneratorWindow>();
+            window.titleContent = new GUIContent("CellBundleGenerator");
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log("CellBundleGeneratorWindow");
+            if(this.target == null)
+            {
+                var masterData = AssetDatabase.LoadAssetAtPath<MasterData>("Assets/HK/AutoAnt/DataSources/Database/MasterData/MasterData.asset");
+                Assert.IsNotNull(masterData, $"{typeof(MasterData).Name}の読み込みに失敗しました");
+                this.target = masterData.CellBundle;
+                Assert.IsNotNull(this.target, $"{typeof(MasterDataCellBundle).Name}の読み込みに失敗しました");
+            }
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("System");
+            EditorGUI.indentLevel++;
+            EditorGUILayout.ObjectField("Target", this.target, typeof(MasterDataCellBundle), false);
+            GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
+        }
+    }
+}
