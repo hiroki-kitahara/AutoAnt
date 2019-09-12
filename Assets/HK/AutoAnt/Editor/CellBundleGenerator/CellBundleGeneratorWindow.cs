eb539357c877507e0b2c887fdb4344b0c1525a48
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-12 23:38:29 +0900

    範囲検出とボタン配置まで

--- Assets/HK/AutoAnt/Editor/CellBundleGenerator/CellBundleGeneratorWindow.cs
+++ Assets/HK/AutoAnt/Editor/CellBundleGenerator/CellBundleGeneratorWindow.cs
@@ -12,7 +12,10 @@ namespace HK.AutoAnt.Editor
     {
         [SerializeField]
         private MasterDataCellBundle target = null;
-        
+
+        [SerializeField]
+        private RectInt range = new RectInt();
+
         [MenuItem("AutoAnt/Tool/CellBundleGenerator")]
         private static void CreateWindow()
         {
@@ -22,13 +25,35 @@ namespace HK.AutoAnt.Editor
 
         private void OnEnable()
         {
-            Debug.Log("CellBundleGeneratorWindow");
             if(this.target == null)
             {
                 var masterData = AssetDatabase.LoadAssetAtPath<MasterData>("Assets/HK/AutoAnt/DataSources/Database/MasterData/MasterData.asset");
                 Assert.IsNotNull(masterData, $"{typeof(MasterData).Name}の読み込みに失敗しました");
                 this.target = masterData.CellBundle;
                 Assert.IsNotNull(this.target, $"{typeof(MasterDataCellBundle).Name}の読み込みに失敗しました");
+
+                this.range = new RectInt();
+                foreach(var r in this.target.Records)
+                {
+                    if(this.range.x > r.Rect.x)
+                    {
+                        this.range.x = r.Rect.x;
+                    }
+                    if(this.range.y > r.Rect.y)
+                    {
+                        this.range.y = r.Rect.y;
+                    }
+                    var width = r.Rect.x + r.Rect.width;
+                    if(this.range.width < width)
+                    {
+                        this.range.width = width;
+                    }
+                    var height = r.Rect.y + r.Rect.height;
+                    if(this.range.height < height)
+                    {
+                        this.range.height = height;
+                    }
+                }
             }
         }
 
@@ -37,7 +62,20 @@ namespace HK.AutoAnt.Editor
             GUILayout.Label("System");
             EditorGUI.indentLevel++;
             EditorGUILayout.ObjectField("Target", this.target, typeof(MasterDataCellBundle), false);
+            EditorGUILayout.RectIntField("Range", this.range);
             GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
+
+            var tempColor = GUI.color;
+            var buttonSize = 12.0f;
+            for (var y = this.range.y; y <= this.range.height; y++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                for (var x = this.range.x; x <= this.range.width; x++)
+                {
+                    GUILayout.Button(new GUIContent(), GUILayout.Width(buttonSize), GUILayout.Height(buttonSize));
+                }
+                EditorGUILayout.EndHorizontal();
+            }
         }
     }
 }
